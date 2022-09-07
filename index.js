c76e675a480ea2738b39faba259708e40df8211d
eli-codes <elitutor0116@gmail.com>
2022-09-07 10:20:42 -0700

    Corrected Task 2

--- index.js
+++ index.js
@@ -51,16 +51,24 @@ Person.prototype.toString = function(){
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car(model, milesPerGallon) {
- this.model = model;
- this.milesPerGallon = milesPerGallon;
- this.tank = 0;
- this.odemeter = 0;
+function Car(model, mpg){
+  this.model = model;
+  this.milesPerGallon = mpg;
+  this.tank = 0;
+  this.odometer = 0;
 }
-
 Car.prototype.fill = function(gallons){
-  if(this.tank.length < 10){
-    this.tank.fill(gallons);
+  this.tank = this.tank + gallons;
+}
+Car.prototype.drive = function(distance){
+  const driveableMiles = this.tank * this.milesPerGallon;
+  if(distance <= driveableMiles){
+    this.odometer = this.odometer + distance;
+    this.tank = this.tank - (distance / this.milesPerGallon);
+  } else{
+    this.odometer = this.odometer + driveableMiles;
+    this.tank = 0;
+    return `I ran out of the fuel at ${this.odometer} miles`;
   }
 }
 
